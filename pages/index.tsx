472e418a4ff6357b7979ce2ff39a07eb0d95c16f
Kingshuk Deb <itskingshukdeb@gmail.com>
2023-01-10 00:56:09 +0530

    Initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <>
+    <> 
       <Head>
         <title>Ecommerce Product</title>
         <meta name="description" content="Created by Kingshuk Deb" />
